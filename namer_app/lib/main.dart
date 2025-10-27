a929b73144388b1761b8d9d300c04293d80adacc
Felipe Guerra <fguerra21@alumnos.utalca.cl>
2025-10-27 12:54:58 -0300

    Cambiar ip directamente desde app.
    permite cambiar la ip desde la configuración en el login.

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:namer_app/pages/login_page.dart';
+import 'package:namer_app/services/api_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ApiService.initBaseUrl();
   runApp(const LossUtalApp());
 }
 
